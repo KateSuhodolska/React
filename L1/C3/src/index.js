a431b083fcf516c538afa1655f378c9aa59f3fb7
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-09-03 19:39:39 +0300

    Update index.js

--- L1/C3/src/index.js
+++ L1/C3/src/index.js
@@ -4,12 +4,11 @@ import "./styles.css";
 
 const rootElement = document.querySelector("#root");
 
-const elem = ( <
-    div className = "greeting" >
-    <
-    div className = "greeting__title" > Hello, world! < /div>{" "} <
-    div className = "greeting__text" > I 'm learning React</div>{" "} <
-    /div>
+const elem = ( 
+ <div className="greeting">
+    <div className="greeting__title">Hello, world!</div>
+    <div className="greeting__text">I'm learning React</div>
+ </div>
 );
 
 ReactDOM.render(elem, rootElement);
