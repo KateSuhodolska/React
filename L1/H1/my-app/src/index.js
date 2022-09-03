ae1207cbebd0682e1b5983b8f30e8e39061387a2
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-09-03 21:19:03 +0300

    lessons

---
+++ L1/H1/my-app/src/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+import ReactDOM from "react-dom";
+import React from "react";
+
+const container = document.querySelector("body");
+
+const elem = <h1 class="title">Todo List</h1>;
+
+ReactDOM.render(elem, container);
