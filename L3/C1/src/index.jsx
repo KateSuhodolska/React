9736c0e8973383ac655fbfa2ba78a868712cd30e
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-09-06 12:48:07 +0300

    lessons

---
+++ L3/C1/src/index.jsx
@@ -0,0 +1,6 @@
+import Search from "./Search.jsx";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.querySelector("#root");
+ReactDOM.render(<Search name="Tom" />, rootElement);
