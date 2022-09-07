79cb35c48d319707b99decd62983e5067d738d09
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-09-07 15:17:08 +0300

    lessons

--- L3/H2/src/Profile.jsx
+++ L3/H2/src/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = (props) => {
   return (
     <div className="profile">
       <div className="profile__name">
-        {`${props.userData.firstName} ${props.userData.lastName}`}{" "}
+        {`${props.userData.firstName} ${props.userData.lastName}`}
       </div>
       <div className="profile__birth">
         {`Was born ${moment(props.userData.birthDay).format("DD MMM YY")} in ${
