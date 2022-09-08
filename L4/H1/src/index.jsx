1caf21dc42c840a5abbcec9b9dcfa893985b1e73
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-09-08 18:09:13 +0300

    lessons

--- L4/H1/src/index.jsx
+++ L4/H1/src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
-import moment from "moment";
 import "./index.scss";
 
 const rootEl = document.querySelector("#root");
