79eaec1ae58e3a712d7f3b4dd1ecb6bfc936fc5c
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-09-22 12:21:39 +0300

    lessons

---
+++ L8/C4/src/User.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class Dimensions extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userData: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch(`https://api.github.com/users/${this.props.userId}`)
+      .then((response) => response.json())
+      .then((userData) => {
+        this.setState({
+          userData,
+        });
+      });
+  }
+
+  render() {
+    return this.state.userData ? (
+      <div className="user">
+        <img
+          alt="User Avatar"
+          src={this.state.userData.avatar_url}
+          className="user__avatar"
+        />
+        <div className="user__info">
+          <span className="user__name">{this.state.userData.name}</span>
+          <span className="user__location">{this.state.userData.location}</span>
+        </div>
+      </div>
+    ) : null;
+  }
+}
+
+export default Dimensions;
