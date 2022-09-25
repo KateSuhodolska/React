20acb45b725e2b4f529d3e7fbb8bfd7e335aabde
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-09-25 11:28:50 +0300

    lessons

---
+++ L9/C1/src/Search.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  search = (event) => {
+    event.preventDefault();
+    alert(this.state.text);
+  };
+
+  handleSubmit = (event) => {
+    this.setState({
+      text: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <form className="search" onSubmit={this.search}>
+        <input
+          type="text"
+          className="search__input"
+          onChange={this.handleSubmit}
+          value={this.state.value}
+        />
+        <button className="search__button" type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+export default Search;
